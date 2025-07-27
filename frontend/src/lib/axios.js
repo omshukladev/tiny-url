960821ff2adbe26d997a2beec65f407fdce05bb7
OmShukla-Dev <omshuklalko@gmail.com>
2025-07-27 22:15:28 +0530

    bug fix 12

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
-import axios from "axios"; 
+import axios from "axios";
 
-const baseURL =
-  import.meta.env.PROD
-    ? import.meta.env.VITE_BACKEND_URL
-    : "http://localhost:4000/";
+const baseURL = import.meta.env.PROD ? "/" : "http://localhost:4000/";
 
 export const axiosInstance = axios.create({
   baseURL,

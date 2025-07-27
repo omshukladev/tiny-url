b437d7d2bea48a037a53882aaf904e8db83946f4
OmShukla-Dev <omshuklalko@gmail.com>
2025-07-27 22:06:47 +0530

    bug fix 11

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   //  Keep this LAST so it doesn’t override `/api/...`
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
   });
 }
